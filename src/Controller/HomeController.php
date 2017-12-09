e96c931856251a05d4f31a815f47c7d1e7e3b3dd
Peter Ton <mail@peterton.nl>
2017-12-09 16:55:32 +0100

    Implement basic search form with bootstrap styling

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,11 +3,21 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        return $this->render('home.html.twig');
+        $form = $this->createFormBuilder()
+            ->add('PlayerId', TextType::class)
+            ->add('save', SubmitType::class, array('label' => 'Go!'))
+            ->getForm();
+
+        return $this->render(
+            'home.html.twig', [
+            'form' => $form->createView(),
+        ]);
     }
 }

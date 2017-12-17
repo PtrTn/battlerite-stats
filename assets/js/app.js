03c4e1b2d74ade036c8608968a15eb2628ded05c
Peter Ton <mail@peterton.nl>
2017-12-17 20:45:40 +0100

    Add champion images to build process

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,29 @@
+// Require images due to lack of asset support.
+require('../images/Lucie.png');
+require('../images/Sirius.png');
+require('../images/Pestilus.png');
+require('../images/Blossom.png');
+require('../images/Iva.png');
+require('../images/Alysia.png');
+require('../images/Rook.png');
+require('../images/Jade.png');
+require('../images/Ruh Kaan.png');
+require('../images/Oldur.png');
+require('../images/Ashka.png');
+require('../images/Varesh.png');
+require('../images/Pearl.png');
+require('../images/Destiny.png');
+require('../images/Taya.png');
+require('../images/Poloma.png');
+require('../images/Croak.png');
+require('../images/Freya.png');
+require('../images/Jumong.png');
+require('../images/Shifu.png');
+require('../images/Ezmo.png');
+require('../images/Raigon.png');
+require('../images/Thorn.png');
+require('../images/Bakko.png');
+
 require('timeago');
 jQuery(document).ready(function () {
     $("time.timeago").timeago();
